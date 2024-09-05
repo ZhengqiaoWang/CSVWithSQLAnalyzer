9f8acde8cf052a50f5fd0b58674071a056910755
王政乔 <me@zhengqiao.wang>
2024-09-05 14:56:43 +0800

    代码格式和功能改进
    - 格式化代码，统一字符串引号和参数排列，提高可读性。
    - 引入自动补全功能，提升命令行输入体验。
    - 优化SQL语句处理，正确显示结果列名和分隔线。
    - 调整数据库连接和查询执行逻辑，确保资源正确管理和SQL执行效率。
    - 改进参数解析和命令行界面，提升用户交互体验。
    - 修正代码注释和文档字符串，确保准确反映代码功能和用法。

--- generate_datefile.py
+++ generate_datefile.py
@@ -2,15 +2,18 @@ import argparse
 import sqlite3
 import os
 import csv
-from typing import Union, List, Dict
+from typing import List, Dict
+
 
 def parse_args():
     # 解析命令行参数
-    parser = argparse.ArgumentParser(description='Generate datefile.txt')
+    parser = argparse.ArgumentParser(description="Generate datefile.txt")
     # 添加参数，解析CSV文件夹路径
-    parser.add_argument('--csv_folder', required=True, type=str, help='CSV folder path')
+    parser.add_argument("--csv_folder", required=True, type=str, help="CSV folder path")
     # 添加参数，解析输出的数据库文件路径
-    parser.add_argument('--output_db', required=True, type=str, help='Output database file path')
+    parser.add_argument(
+        "--output_db", required=True, type=str, help="Output database file path"
+    )
     return parser.parse_args()
 
 
@@ -19,10 +22,11 @@ def scan_csv_files(csv_folder_path: str) -> Dict[str, str]:
     csv_filename_path_dict = {}
     for root, _, files in os.walk(csv_folder_path):
         for file in files:
-            if not file.endswith('.csv'):
+            if not file.endswith(".csv"):
                 continue
             csv_filename_path_dict[file] = os.path.join(root, file)
     return csv_filename_path_dict
+
 
 def get_table_name(file_name: str) -> str:
     """
@@ -40,39 +44,46 @@ def get_table_name(file_name: str) -> str:
     # 分割文件名，取第一个部分作为表名
     return os.path.splitext(file_name)[0]
 
+
 # sqlite3创建表
 def create_table(conn: sqlite3.Connection, table_name: str, columns: List[str]):
     cursor = conn.cursor()
-    sql = "Create Table if not exists {} ({})".format(table_name, ", ".join([f'{col} TEXT' for col in columns]))
-    print("\t",sql)
+    sql = "Create Table if not exists {} ({})".format(
+        table_name, ", ".join([f"{col} TEXT" for col in columns])
+    )
+    print("\t", sql)
     cursor.execute(sql)
     conn.commit()
     cursor.close()
 
+
 # 将csv数据写入数据库
 def write_csv_data_to_db(conn: sqlite3.Connection, table_name: str, csv_file_path: str):
     cursor = conn.cursor()
-    with open(csv_file_path, 'r') as f:
+    with open(csv_file_path, "r") as f:
         reader = csv.reader(f)
-        headers = [col.strip().strip("\"") for col in next(reader)]
+        headers = [col.strip().strip('"') for col in next(reader)]
         create_table(conn, table_name, headers)
         for row in reader:
             sql = f"INSERT INTO {table_name} VALUES ({', '.join(['?'] * len(row))})"
-            cursor.execute(sql, [x.strip().strip("\"") for x in row])
+            cursor.execute(sql, [x.strip().strip('"') for x in row])
             conn.commit()
     cursor.close()
     return True
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = parse_args()
-    
+
     # 创建sqlite数据库连接
     conn = sqlite3.connect(args.output_db)
     for file_name, file_path in scan_csv_files(args.csv_folder).items():
         table_name = get_table_name(file_name)
-        print("Creating and Writing data to table: {} ({})".format(table_name, file_name))
-        write_csv_data_to_db(conn,table_name, file_path)
-    
+        print(
+            "Creating and Writing data to table: {} ({})".format(table_name, file_name)
+        )
+        write_csv_data_to_db(conn, table_name, file_path)
+
     conn.close()
 
     print()
